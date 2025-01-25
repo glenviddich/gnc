146b16a8011b74e5c2232d0e584d1cbd9ce9771a
glenviddich <dennis.reiss@gmail.com>
2025-01-25 17:00:28 +0100

    Update __init__.py

--- custom_components/gnc/__init__.py
+++ custom_components/gnc/__init__.py
@@ -1,10 +1,36 @@
 import logging
-
-from homeassistant.core import HomeAssistant
+import subprocess
+import os
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the GN Server integration."""
-    _LOGGER.info("GN Server Integration has been set up!")
+DOMAIN = "gnc"
+
+def setup(hass, config):
+    """Set up the GNC integration."""
+    _LOGGER.info("Setting up GNC integration...")
+
+    # Pfad zur JAR-Datei
+    jar_path = os.path.join(
+        hass.config.path("custom_components"), DOMAIN, "gn_server.jar"
+    )
+
+    if not os.path.isfile(jar_path):
+        _LOGGER.error("gn_server.jar not found at %s", jar_path)
+        return False
+
+    _LOGGER.info("Found gn_server.jar at %s", jar_path)
+
+    # Versuche, den Server zu starten
+    try:
+        command = f"java -jar {jar_path}"
+        _LOGGER.info("Executing command: %s", command)
+        subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        _LOGGER.info("GNC server started successfully.")
+    except Exception as e:
+        _LOGGER.error("Failed to start GNC server: %s", str(e))
+        return False
+
     return True
